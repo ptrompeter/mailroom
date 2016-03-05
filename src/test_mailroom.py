13ac51b94b23daeb353eb55b9b9dd4d72cfe34a1
Mike <mikekh@uw.edu>
2016-03-04 17:35:52 -0800

    Added more tests. Fixed sortbydonation

--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -5,14 +5,33 @@ data = {('Roberts', 'John'): [20.00, 15.00],
         ('asdf', 'brice'): [2.35],
         }
 
+
 def test_sortbydonation():
     """Test Sort By donation function in mailroom"""
     from mailroom import sort_by_donation
     assert type(sort_by_donation(data)) == list
-    assert len(sort_by_donation(data)) == 3
+    #assert len(sort_by_donation(data)) == 3
+    data2 = sort_by_donation(data)
+    for i in range(len(data2) - 1):
+        assert data2[i] > data2[i + 1]  or len(data2) < 2
+
 
 def test_getname():
-	from mailroom import get_name
-	name = 'Mike Kang Harrison'
-	assert type(get_name(name)) == tuple
-	assert len(get_name(name)) == 2
+    """Test Get Name function in mailroom"""
+    from mailroom import get_name
+    name = 'Mike Kang Harrison'
+    assert type(get_name(name)) == tuple
+    assert len(get_name(name)) == 2
+
+
+def test_menuprompot():
+	""" Test menu prompt"""
+	from mailroom import menu_prompt
+	assert menu_prompt('2') == '2'
+
+
+def test_getdonation():
+	"""Test Get Donation"""
+	from mailroom import get_donation
+	assert get_donation('2') == 2.0
+
